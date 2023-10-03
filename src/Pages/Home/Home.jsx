9be185fa43993e3fad754d5a952668d664d8ca14
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-10-04 01:15:31 +0600

    Created navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import BreakingNews from "../../Components/BreakingNews/BreakingNews";
 import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
 
 
 const Home = () => {
     return (
-      <div className="font-poppins">
+      <div className="font-poppins space-y-10">
             <Header></Header>
             <BreakingNews></BreakingNews>
+            <Navbar></Navbar>
       </div>
     );
 };
