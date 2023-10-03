58c4bbdd29bda59bf1c43d4a34d87096599b72f5
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-10-04 01:05:11 +0600

    Added Marquee in the breaking news

--- src/Components/BreakingNews/BreakingNews.jsx
+++ src/Components/BreakingNews/BreakingNews.jsx
@@ -1,13 +1,23 @@
-
+import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
-    return (
-      <div className="h-[80px] bg-[#F3F3F3] flex items-center">
-        <button className="btn rounded-none bg-[#D72050] text-white ml-4">
-          Latest
-        </button>
-      </div>
-    );
+  return (
+    <div className="h-[80px] bg-[#F3F3F3] flex items-center">
+      <button className="btn rounded-none bg-[#D72050] text-white ml-4">
+        Latest
+      </button>
+      <Marquee
+        gradient="true"
+        pauseOnClick="true"
+        gradientColor="#D72050"
+        speed={130}
+      >
+        I can be a React component, multiple React components, or just some
+        text.Discover thousands of options, easy to customize layouts, one-click
+        to import demo and much more.
+      </Marquee>
+    </div>
+  );
 };
 
 export default BreakingNews;
