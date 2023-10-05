8e6f32d6cb5062e04c45fbba8037c7d3446825ad
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-10-05 16:59:05 +0600

    Find us section

--- src/Components/Leftbar/LeftBar.jsx
+++ src/Components/Leftbar/LeftBar.jsx
@@ -1,8 +1,8 @@
-import { FaGoogle , FaGithub } from "react-icons/fa";
+import { FaGoogle , FaGithub , FaFacebook ,FaInstagram ,FaTwitter} from "react-icons/fa";
 
 const LeftBar = () => {
   return (
-    <div>
+    <div className="space-y-14 ">
       <div className="flex flex-col gap-4">
         <p className="text-lg font-bold">Login With</p>
         <button className="btn lowercase text-blue-500 border-blue-500 bg-none">
@@ -14,7 +14,30 @@ const LeftBar = () => {
           Login with Github
         </button>
       </div>
-      <div></div>
+      <div className="">
+        <p className="mb-4 text-lg font-bold">Find Us On</p>
+        <div className="w-full border-2 flex justify-start items-center h-[50px] rounded-md rounded-b-none">
+          {" "}
+          <button>
+            <FaFacebook className="bg-white text-blue-700 inline ml-4"></FaFacebook>{" "}
+            Facebook
+          </button>
+        </div>
+        <div className="w-full border-r-2 border-l-2 flex justify-start items-center h-[50px] ">
+          {" "}
+          <button>
+            <FaInstagram className="bg-white text-blue-700 inline ml-4"></FaInstagram>{" "}
+            Instagram
+          </button>
+        </div>
+        <div className="w-full border-2 flex justify-start items-center h-[50px] rounded-md rounded-t-none">
+          {" "}
+          <button>
+            <FaTwitter className="bg-white text-blue-700 inline ml-4"></FaTwitter>{" "}
+            Twitter
+          </button>
+        </div>
+      </div>
       <div></div>
     </div>
   );
